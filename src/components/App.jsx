be31a20a65752bf57f71eac6bd98e7157cf17def
Alex Garcia <g.nightster11@gmail.com>
2019-03-07 14:32:07 -0800

    add parallax to home component and picture. also deleted other home pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,60 +4,29 @@ import Home from './Home';
 import Teams from './Teams';
 import Contact from './Contact';
 import Events from './Events';
-import About from './About';
-import Sponsor from './Sponsor';
-import MidHome from './MidHome';
-import MidLowHome from './MidLowHome';
-import News from './News';
 import { Switch, Route } from 'react-router-dom';
 import { ParallaxProvider } from 'react-scroll-parallax';
-import { Parallax } from 'react-scroll-parallax';
-
 import Error404 from './Error404';
 
 
-const ParallaxImage = () => (
-  <Parallax className="custom-class" y={[-20, 20]} tagOuter="figure">
-    <img src={kick} />
-  </Parallax>
-);
 
-
-class App extends React.Component {
-
-  render(){
+function App(){
     
-    return (
-      <div>
-        <Header/>
-        <ParallaxProvider>
-          <div>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/Teams' component={Teams} />
-              <Route path='/Events' component={Events} />
-              <Route path='/Contact' component={Contact} />
-              <Route component={Error404} />
-            </Switch>
-          </div>
-          {/* //need to add state for hide show functionality for this components */}
-          <div>
-            <About/>
-          </div>
-          <div>
-            <MidHome/>
-          </div>
-          <div>
-            <News/>
-          </div>
-          <MidLowHome/>
-          <div>
-            <Sponsor/>
-          </div>
-        </ParallaxProvider>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header/>
+      <ParallaxProvider>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/Teams' component={Teams} />
+            <Route path='/Events' component={Events} />
+            <Route path='/Contact' component={Contact} />
+            <Route component={Error404} />
+          </Switch>
+      </ParallaxProvider>
+    </div>
+  );
 }
+
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,68 @@
 import React from 'react';
+import About from './About';
+import News from './News';
+import Sponsor from './Sponsor';
+import { Parallax } from 'react-scroll-parallax';
 import ball from '../assets/rugbyBall.jpg';
+import kick from '../assets/kick.jpg';
+import scrum from '../assets/scrum.jpg';
 
+const imgTest={
+  width:'100vw',
+  height:'100vh'
+}
+
+const test ={
+  backgroundColor:'grey',
+  zIndex:'-1'
+}
 
 function Home(){
   return (
     <div>
-      <img  src={ball} />
+      
+      <div>
+     <Parallax
+            className="firstParallax"
+            offsetYMax={20}
+            offsetYMin={-20}
+            slowerScrollRate
+            tag="figure"
+        >
+          <img style={imgTest} src={ball} />
+          </Parallax>
+      </div>
+          <div style={test}>
+          <About/>
+        </div>
+        <div>
+        <Parallax
+            className="firstParallax"
+            offsetYMax={20}
+            offsetYMin={-20}
+            slowerScrollRate
+            tag="figure"           
+        >
+          <img style={imgTest} src={kick} />
+          </Parallax>
+        </div>
+        <div style={test}>
+          <News/>
+        </div>
+        <div>
+        <Parallax
+            className="firstParallax"
+            offsetYMax={20}
+            offsetYMin={-20}
+            slowerScrollRate
+            tag="figure"
+        >
+          <img style={imgTest} src={scrum} />
+          </Parallax>
+        </div>
+        <div style={test}>
+        <Sponsor/>
+        </div>
     </div>
   );
 }
