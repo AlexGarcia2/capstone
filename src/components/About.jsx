3031597ed16b667c858e9a087d30e6d5394871b1
Alex Garcia <g.nightster11@gmail.com>
2019-03-08 08:46:52 -0800

    add some styling to components in home but need to find different pictures

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
+const card={
+  border='2px 1px grey',
+  backgroundColot='#FF875E'
+}
 
 
 function About(){
   return (
-    <div >
+    <div style={card}>
+      <div>
+
       <h1>mission statement</h1>
+
+      <div>
+        <p>Salem Oregon's premier Men's and Women's Rugby Football Club!!!! Striving for success both on the field and off.Based out of Salem Oregon, we are a Non-Profit organization promoting the growth of Rugby in the Salem, Keizer and surrounding areas
+Experienced or not anyone is welcome to join us</p>
+      </div>
+
+
+      </div>
+
 
     </div>
   );
