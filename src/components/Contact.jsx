3428ec0e0a8262a83af90cd5e1a944931bef24a0
Alex Garcia <g.nightster11@gmail.com>
2019-03-08 16:45:37 -0800

    add basic form for contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+const card={
+  backgroundColor:'#fff9e7'
+}
+
 
 function Contact(){
   return (
-    <div>
-        HELLO
+    <div style={card}>
+      <form action="">
+          <div>
+            <h2>Please Contact Us</h2>
+          </div>
+          <div>
+            <input type="text" placeholder="First"/>  <input type="text" placeholder="Last"/>
+          </div>
+          <div>
+          <input type="text" placeholder="Email"/>
+          </div>
+          <div>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+        </form>
     </div>
   );
 }
