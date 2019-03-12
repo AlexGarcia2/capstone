86f38e53b6b91140b88486bd57cd635e59a33caf
Student <example@example.com>
2019-03-12 08:26:46 -0700

    took out style of app on the master

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,9 +14,9 @@ import blackWhite from '../assets/backgroundBlackWhite.jpg';
 
 
 function App(){
-    
+
   return (
-    <div style={blackWhite} >
+    <div>
       <div >
      <Header/>
       </div>
