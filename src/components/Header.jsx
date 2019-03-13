63cf32a45e342ba4373a21fdb1f1bac8edb779da
Student <example@example.com>
2019-03-13 16:25:48 -0700

    changed styling and font of name and other styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,29 +3,34 @@ import { Link } from 'react-router-dom';
 
 
 const navBar={
-  position:'relative',
   padding:'10px',
   backgroundColor:'white',
   borderTop:'2px solid black',
   borderBottom:'2px solid black',
   display:'grid',
-  gridTemplateColumns:'auto auto'
+  gridTemplateColumns:'auto auto',
+  width:'100%'
 }
 const bottons={
-  position:'relative',
-  left:'400px',
-  top:'40px', 
+  float:'right',
+  marginTop:'70px',
+  marginLeft:'900px'
 }
 const buttonSpace={
   color:'black',
   margin:'10px'
 }
 
+const title={
+  marginLeft:'10px',
+  fontFamily:'impact'
+}
+
 function Header(){
   return (
     <div style={navBar}>
     <div>
-      <h1>Salem Rugby</h1>
+      <h1 style={title}>Salem Rugby</h1>
     </div>
     <div style={bottons}>
      <Link style={buttonSpace} to="/">Home</Link>  <Link style={buttonSpace} to="/Contact">Contact</Link>  <Link style={buttonSpace} to="/Teams">Teams</Link>  <Link style={buttonSpace} to="/Events">Events</Link>
