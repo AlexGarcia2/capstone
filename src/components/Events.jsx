295ce64920de613f8106616f9395f447e779512f
Student <example@example.com>
2019-03-13 16:24:25 -0700

    add calendarBoard component and spacing and styling

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import News from'./News';
+import CalendarBoard from './CalendarBoard';
 
-
+const title={
+    margin:'50px',
+    background: 'rgba(255, 0, 0, 0.7)',
+    width:'500px',
+    paddingLeft:'5px',
+  }
 
 function Events(){
   return (
     <div>
-      <h1>Events information</h1>
+      <h1 style={title} >Events information</h1>
+      <CalendarBoard/>
       <div>
 
       </div>
